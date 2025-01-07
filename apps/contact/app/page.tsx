cb3c04d4f085b2dce103d60a27dca46e72346dec
Rasel Hossain <rasel@stacklearner.com>
2025-01-07 23:53:25 +0600

    update

--- apps/contact/app/page.tsx
+++ apps/contact/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Contact Home Page</h1>;
+  return (
+    <>
+      <h1>Contact Home Page</h1>
+
+      <Link href="/contact">Contact Form</Link>
+    </>
+  );
 }
