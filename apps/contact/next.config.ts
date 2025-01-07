2e20ba4593cd3cd061b56c9adbef570719965b86
Rasel Hossain <rasel@stacklearner.com>
2025-01-08 03:04:57 +0600

    update

--- apps/contact/next.config.ts
+++ apps/contact/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@repo/ui"],
+
+  basePath: "/contact",
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "http://localhost:3001",
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
--- apps/main/next.config.ts
+++ apps/main/next.config.ts
@@ -1,27 +1,36 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	experimental: {
-		ppr: true,
-		dynamicIO: true,
-	},
-	images: {
-		remotePatterns: [
-			{
-				hostname: 'github.com',
-				protocol: 'https',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				hostname: 'images.unsplash.com',
-				protocol: 'https',
-				port: '',
-				pathname: '/**',
-			},
-		],
-	},
+  /* config options here */
+  experimental: {
+    ppr: true,
+    dynamicIO: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "github.com",
+        protocol: "https",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        hostname: "images.unsplash.com",
+        protocol: "https",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/contact",
+        destination: "https://mono-minia-psi.vercel.app/contact",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
