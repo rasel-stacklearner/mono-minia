b696f5ab7f3e6dbe33afff9c98172ee6804bea54
Rasel Hossain <rasel@stacklearner.com>
2025-01-09 17:25:40 +0600

    update

--- apps/main/next.config.ts
+++ apps/main/next.config.ts
@@ -23,29 +23,57 @@ const nextConfig: NextConfig = {
     ],
   },
 
+  // async rewrites() {
+  //   // console.log("APP_URL 😀", process.env.APP_URL);
+  //   return [
+  //     {
+  //       source: "/contact/:path*",
+  //       // destination: `http://localhost:3000/:path*`,
+  //       destination: `https://mono-minia-psi.vercel.app/:path*`,
+  //     },
+  //     {
+  //       source: "/about/:path*",
+  //       // destination: `http://localhost:3002/:path*`,
+  //       destination: `https://mono-minia-vert.vercel.app/:path*`,
+  //     },
+
+  //     {
+  //       source: "/about",
+  //       // destination: `http://localhost:3002/:path*`,
+  //       destination: `https://mono-minia-vert.vercel.app`,
+  //     },
+
+  //     {
+  //       source: "/contact",
+  //       // destination: `http://localhost:3000/:path*`,
+  //       destination: `https://mono-minia-psi.vercel.app`,
+  //     },
+  //   ];
+  // },
   async rewrites() {
-    // console.log("APP_URL 😀", process.env.APP_URL);
     return [
       {
         source: "/contact/:path*",
-        // destination: `http://localhost:3000/:path*`,
         destination: `https://mono-minia-psi.vercel.app/:path*`,
       },
       {
+        source: "/contact/_next/:path*",
+        destination: `https://mono-minia-psi.vercel.app/_next/:path*`,
+      },
+      {
         source: "/about/:path*",
-        // destination: `http://localhost:3002/:path*`,
         destination: `https://mono-minia-vert.vercel.app/:path*`,
       },
-
+      {
+        source: "/about/_next/:path*",
+        destination: `https://mono-minia-vert.vercel.app/_next/:path*`,
+      },
       {
         source: "/about",
-        // destination: `http://localhost:3002/:path*`,
         destination: `https://mono-minia-vert.vercel.app`,
       },
-
       {
         source: "/contact",
-        // destination: `http://localhost:3000/:path*`,
         destination: `https://mono-minia-psi.vercel.app`,
       },
     ];
